1f9eb8a807112bbe4185ed51898f75ad73643e22
Louis Thie <louisthie@orcon.net.nz>
2015-11-24 02:42:09 +1300

    uploaded, somehow things are working

--- Nandoso/Nandoso/js/internet.js
+++ Nandoso/Nandoso/js/internet.js
@@ -1,19 +1,16 @@
 ﻿function Internet() {
-    alert("internet");
     var xhttp = new XMLHttpRequest();
 
     //This gets triggered when the state of the xhttp object changes
     xhttp.onreadystatechange = function () {
         // 4 - repsonse is ready, 200 success code
-        alert(xhttp.readyState);
-        if (xhttp.readyState === 4) {
-            alert("json");
-            alert(xhttp.responseText);
+        if (xhttp.readyState === 4 && xhttp.status === 201) {
+            alert("Your review has been posted.");
         }
     }
 
     // Build up our request and send it - true for async
-    xhttp.open("GET", "http://localhost:24530/api/Reviews/1", true);
-    xhttp.setRequestHeader("Content-type", "application/xml");
+    xhttp.open("POST", "api/Reviews", true);
+    xhttp.setRequestHeader('Data-Type', 'text/json');
     xhttp.send();
 }
